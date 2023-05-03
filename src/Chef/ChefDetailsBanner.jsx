85bb38f8bba75c41f3ad476782e969dbb60c5e43
sayemhaque <mdsayemofficial1000@gmail.com>
2023-05-03 11:33:42 +0600

    fixed chefBanner responsive issue

--- src/Chef/ChefDetailsBanner.jsx
+++ src/Chef/ChefDetailsBanner.jsx
@@ -5,11 +5,11 @@
 const ChefDetailsBanner = ({data}) => {
     const {chefPicture,chefName,bio,likes,recipes,experience} = data;
     return (
-        <div className="min-h-[300px] bg-gray-900 px-16 py-8">
+        <div className="lg:min-h-[300px] bg-gray-900 px-5 lg:px-16 py-8">
             <div className="flex flex-col justify-around lg:flex-row items-center">
-                <img src={chefPicture} className="max-w-lg h-[500px] rounded-lg shadow-2xl object-contain" />
-                <div className="max-w-md">
-                    <h1 className="text-5xl text-gray-200 font-bold">{chefName}</h1>
+                <img src={chefPicture} className="w-full md:max-w-lg h-auto lg:h-[500px] rounded-lg shadow-2xl object-contain" />
+                <div className="w-full md:max-w-md mt-4">
+                    <h1 className="text-2xl lg:text-5xl text-gray-200 font-bold">{chefName}</h1>
                     <p className="py-6 text-gray-300 font-semibold">{bio}</p>
                     <p className="py-1 text-gray-300 font-semibold">Likes : {likes}</p>
                     <p className="py-1 text-gray-300 font-semibold">Total Recipes: {recipes.length}</p>
--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -10,8 +10,8 @@ const Navbar = () => {
     .catch(error => console.log(error))
   }
   return (
-    <div className=" bg-slate-700 text-white shadow-lg ">
-      <div className="navbar md:max-w-[1200px] mx-auto lg:px-24">
+    <div className=" bg-slate-700 sticky top-0 z-50 text-white shadow-lg ">
+      <div className="navbar   md:max-w-[1200px] mx-auto lg:px-24">
         <div className="navbar-start py-3">
           <div className="dropdown">
             <label tabIndex={0} className="btn btn-ghost lg:hidden">
