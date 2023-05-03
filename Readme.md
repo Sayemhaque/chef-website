39dc02fbb68446f1d3f24fb9c210abef741c2dd0
sayemhaque <mdsayemofficial1000@gmail.com>
2023-05-03 14:19:12 +0600

    readme file added

---
+++ Readme.md
@@ -0,0 +1,13 @@
+# Live Link : <https://repice-app-6cac9.web.app/>
+
+
+1. Features : 
+    1. This is a single page application.
+    2. It has dynamic routing.
+    3. It has firebase authentication.
+    4. Chef Details page is protected.
+    4. If a user if logged in and clicks on the view recipe button , he/she will redirect to
+    the chef details page.
+    5. if user does not logged in he/she will redirect to the login page.
+    6. This website has backend.We are getting data from backend dummy json data.
+    
--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -2,23 +2,23 @@
 
 const Blog = () => {
     return (
-        <div className="flex flex-col gap-10 px-12 max-w-[800px] mx-auto py-12">
+        <div className="flex flex-col gap-10 px-5 md:px-12 md:max-w-[800px] mx-auto py-12">
             <div>
-                <h3 className="text-3xl font-bold">1.Differences between uncontrolled and controlled components</h3>
+                <h3 className="text-2xl md:text-3xl font-bold">1.Differences between uncontrolled and controlled components</h3>
                 <p className="font-bold mt-3">In controlled component the form input element’s values and mutations are controlled by event handlers and the value of the input element is always changes by the state.</p>
                 <p className="font-bold mt-3">In uncontrolled component the form input element’s do not use state or any event handler. This type of component doesn’t care about an input element’s real-time value changes</p>
             </div>
             <div>
-                <h3 className="text-3xl font-bold">2.How to validate React props using PropTypes</h3>
+                <h3 className="text-2xl md:text-3xl font-bold">2.How to validate React props using PropTypes</h3>
                 <p className="font-bold mt-3">PropTypes is a type-checking library that comes with React. It allows us to specify the types of the props that your components receive, which helps us catch bugs early and improve the overall quality of our code.we need to define the propTypes property on the component function and assign it an object that describes the expected types of the props.photoTypes can be string,boolean,number etc.
                 </p>
             </div>
             <div>
-                <h3 className="text-3xl font-bold">What is a custom hook, and why will you create a custom hook</h3>
+                <h3 className="text-2xl md:text-3xl font-bold">What is a custom hook, and why will you create a custom hook</h3>
                 <p className="font-bold mt-3">Custom hook is a reuseable function. We create custom hook.Custom hook starts with "use".I will create Custom hooks to reduce code duplication and improve code reusability and When i will  find myselfself repeating the same code in multiple components.</p>
             </div>
             <div>
-                <h3 className="text-3xl font-bold">Difference between nodejs and express js.</h3>
+                <h3 className="text-2xl md:text-3xl font-bold">Difference between nodejs and express js.</h3>
                 <p className="font-bold mt-3">Node.js is a run-time environment created to run JavaScript on the server side.
                     Express.js is a framework for Node.js.Express.js will help us to organize out application better and it also provide us many features.</p>
             </div>
